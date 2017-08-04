25e5fc07cc206b8919822bd0ad364b24f69c5b0e
Al Zohali <zohl@fmap.me>
2017-08-04 16:54:18 +0300

    added Protocol.Handshake

--- solution/src/DNIWE/Punt/Interface/Protocol.hs
+++ solution/src/DNIWE/Punt/Interface/Protocol.hs
@@ -1,14 +1,41 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE LambdaCase #-}
 
 
 module DNIWE.Punt.Interface.Protocol where
  -- export all
 
-import Data.Aeson ()
+import Control.Monad (liftM2)
+
+import Data.Aeson (ToJSON(..), FromJSON(..), Value(..), object, (.=), (.:), (.:?))
+import Data.Aeson.Types (typeMismatch)
+
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
 import qualified Data.Text as T
+
+{- TODO
+
+  * issues
+
+  * roadmap
+    - [ ] message
+
+    - [ ] online mode
+      - [X] handshake
+      - [ ] setup
+      - [ ] gameplay
+      - [ ] scoring
+
+    - [ ] offline mode
+
+
+-}
+
 
 data Message = Message {
     msgNum  :: Int
@@ -16,6 +43,28 @@ data Message = Message {
   } deriving (Show, Eq, Generic)
 
 
+data Direction = FromServer | ToServer deriving (Show, Eq)
 
 
+-- - P -> S: {"me" : name}
+-- - S -> P: {"you" : name}
+data Handshake = Handshake {
+    handshakeDirection :: Direction
+  , handshakeName      :: Text
+  } deriving (Show, Eq)
+
+
+instance ToJSON Handshake where
+  toJSON Handshake {..} = object [fieldName .= handshakeName] where
+    fieldName = case handshakeDirection of
+      FromServer -> "you"
+      ToServer   -> "me"
+
+instance FromJSON Handshake where
+  parseJSON (Object v) = liftM2 (,) (v .:? "me") (v .:? "you") >>= \case
+    (Just name, Nothing) -> return $ Handshake { handshakeDirection = ToServer,   handshakeName = name }
+    (Nothing, Just name) -> return $ Handshake { handshakeDirection = FromServer, handshakeName = name }
+    _                    -> fail "Handshake :: unexpected structure"
+
+  parseJSON invalid    = typeMismatch "Handshake" invalid
 
--- solution/tests/protocol/Main.hs
+++ solution/tests/protocol/Main.hs
@@ -1,11 +1,41 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 import Data.Text (Text)
 import qualified Data.Text as T
 
+import DNIWE.Punt.Interface.Protocol (
+    Direction(..)
+  , Handshake(..))
+
 import Test.Hspec (Expectation, Spec, hspec, describe, it, shouldSatisfy, shouldBe)
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck (Gen, Arbitrary(..), sized, resize, oneof, choose, suchThat, frequency, arbitrarySizedNatural, listOf, listOf1, elements)
+
+
+-- inClass :: String -> Gen Char
+-- inClass = oneof . inClass' where
+--   inClass' (x:'-':y:xs) = (choose (x, y)):(inClass' xs)
+--   inClass' (x:xs)       = (return x):(inClass' xs)
+--   inClass' []           = []
+--
+-- charASCII :: Gen Char
+-- charASCII = inClass [chr 32, '-', chr 127]
+--
+--
+-- arbitraryString :: Gen Char -> Gen String
+-- arbitraryString c = sized $ \n -> sequence . replicate n $ c
+--
+--
+--
+-- instance Arbitrary Direction where
+--   arbitrary = elements [FromServer, ToServer]
+--
+-- instance Arbitrary Handshake where
+--   arbitrary = do
+--     handshakeDirection <- arbitrary
+--     handshakeName      <- concatMap (\c -> if c `elem` "\"\\" then ['\\', c] else [c]) <$> (arbitraryString charASCII)
+--     return Handshake {..}
 
 
 
@@ -14,8 +44,3 @@ main = hspec spec
 
 spec :: Spec
 spec = return ()
-
-  -- do
-  -- describe "pgTag" $ do
-  --   it $
-  --   prop $
