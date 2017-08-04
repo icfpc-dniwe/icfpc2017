cf23bae68b5aea04ec8561fb54ee91fa38fd4f61
Al Zohali <zohl@fmap.me>
2017-08-04 17:53:29 +0300

    added Parser.Setup

--- solution/src/DNIWE/Punt/Interface/Protocol.hs
+++ solution/src/DNIWE/Punt/Interface/Protocol.hs
@@ -8,10 +8,12 @@
 module DNIWE.Punt.Interface.Protocol where
  -- export all
 
+import Control.Arrow ((&&&))
 import Control.Monad (liftM2)
 
-import Data.Aeson (ToJSON(..), FromJSON(..), Value(..), object, (.=), (.:), (.:?))
-import Data.Aeson.Types (typeMismatch)
+import Data.Aeson (ToJSON(..), FromJSON(..), Value(..), genericParseJSON, object, (.=), (.:), (.:?))
+import Data.Aeson.Types (typeMismatch, Parser, Options(..), defaultOptions)
+import Data.Char(isUpper, toLower)
 
 import Data.Text (Text)
 import GHC.Generics (Generic)
@@ -52,7 +54,7 @@ data Handshake
 
 
 instance ToJSON Handshake where
-  toJSON (HandshakeRequest {..})  = object ["me".= hrName]
+  toJSON (HandshakeRequest {..})  = object ["me" .= hrName]
   toJSON (HandshakeResponse {..}) = object ["you" .= hrName]
 
 instance FromJSON Handshake where
@@ -61,4 +63,70 @@ instance FromJSON Handshake where
     (Nothing, Just name) -> return $ HandshakeResponse name
     _                    -> fail "Handshake :: unexpected structure"
 
-  parseJSON invalid    = typeMismatch "Handshake" invalid
+  parseJSON invalid = typeMismatch "Handshake" invalid
+
+
+
+dropPrefix :: String -> String
+dropPrefix "" = ""
+dropPrefix (c:t)
+  | isUpper c = toLower c : t
+  | otherwise = dropPrefix t
+
+dropPrefixOptions :: Options
+dropPrefixOptions = defaultOptions { fieldLabelModifier = dropPrefix }
+
+
+type SiteId = Int
+
+data Site = Site {
+    siteId :: SiteId
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON Site where
+  parseJSON = genericParseJSON dropPrefixOptions
+
+  
+data River = River {
+    riverSource :: SiteId
+  , riverTarget :: SiteId
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON River where
+  parseJSON = genericParseJSON dropPrefixOptions
+
+
+data Map = Map {
+    mapSites  :: [(Int, Bool)] -- TODO: replace with custom types
+  , mapRivers :: [(Int, Int)]  -- TODO: replace with custom types
+  } deriving (Show, Eq)
+  
+instance FromJSON Map where
+  parseJSON (Object v) = do
+    sites  <- (v .: "sites")  :: (Parser [Site])
+    rivers <- (v .: "rivers") :: (Parser [River])
+    mines  <- (v .: "mines")  :: (Parser [SiteId])
+
+    let mapSites = map ((id &&& (`elem` mines)) .  siteId) sites
+    let mapRivers = map (riverSource &&& riverTarget) rivers
+    return Map {..}
+
+
+-- S -> P: {"punter" : p, "punters" : n, "map" : map}
+-- P -> S: {"ready" : p}
+data Setup
+  = SetupRequest  { srPunter :: Int, srPunters :: Int, srMap :: Map }
+  | SetupResponse { srPunter :: Int }
+    deriving (Show, Eq, Generic)
+
+instance ToJSON Setup where
+  toJSON (SetupResponse {..}) = object ["ready" .= srPunter ]
+
+instance FromJSON Setup where
+  parseJSON (Object v) = do
+    srPunter  <- v .: "punter"
+    srPunters <- v .: "punters"
+    srMap     <- v .: "map"
+    return SetupRequest {..}
+
+  parseJSON invalid = typeMismatch "Setup" invalid
