f8e90ab12c91075051e4af3b9199f3cd6aa4b1b5
Nikolay Amiantov <ab@fmap.me>
2017-08-05 02:48:47 +0300

    Remove extra nix files

--- solution/default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, aeson, base, hspec, QuickCheck, stdenv, text }:
-mkDerivation {
-  pname = "solution";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ aeson base text ];
-  testHaskellDepends = [ base hspec QuickCheck text ];
-  license = stdenv.lib.licenses.bsd3;
-}
