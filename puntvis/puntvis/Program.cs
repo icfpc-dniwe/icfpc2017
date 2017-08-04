401a716f1d7c33dab4a3cb4d0872ee0606f44c21
dantragof <sergs933@gmail.com>
2017-08-04 15:56:21 +0300

    [+] task.pdf [+] c# gitignore [+] puntvis skeleton

---
+++ puntvis/puntvis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puntvis
+{
+	class Program
+	{
+		const string ServerPrefix = "<-";
+		const string ClientPrefix = "->";
+
+		static void Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				Console.WriteLine("you what");
+				return;
+			}
+			var fname = args[1];
+			var states = File.ReadAllLines(args[1])
+				.Where(x => x.StartsWith(ServerPrefix))
+				.Skip(1) // you: alice
+				.Select(x => x.Substring(ServerPrefix.Length + 1))
+				.ToList();
+
+		}
+	}
+}
