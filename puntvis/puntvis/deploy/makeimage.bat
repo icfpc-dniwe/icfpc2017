5bc6102894e57bc15174bca3f3d160aec83b44be
dantragof <sergs933@gmail.com>
2017-08-04 22:31:30 +0300

    *

--- puntvis/puntvis/deploy/makeimage.bat
+++ puntvis/puntvis/deploy/makeimage.bat
@@ -1,15 +1,21 @@
 @echo off
 if [%1]==[] goto :usage
 
+set PUNTVIS=bin\puntvis.exe
+set IMAGES_DIR=%1-images
+set GRAPHVIZ_DIR=%1-graphviz
+set DOT="dot.exe"
+set FMT=png
+set DPI=250
+@echo using DPI %DPI%. if it's too large -- configure your own
 
 @echo reading log...
-bin\puntvis.exe %1
-rmdir %1-images /q /s
-mkdir %1-images
+"%PUNTVIS%" %1
+rmdir %IMAGES_DIR% /q /s
+mkdir %IMAGES_DIR%
 
 @echo generating images...
-for %%i in (%1-graphviz/*) do fdp.exe -Tsvg %1-graphviz/%%i -o %1-images/%%i.svg
-
+for %%i in (%GRAPHVIZ_DIR%/*) do %DOT% -Gdpi="%DPI%" -T%FMT% %GRAPHVIZ_DIR%/%%i -o %IMAGES_DIR%/%%i.%FMT%
 
 goto :eof
 :usage
