0159d7e08e434138e1e9b4fef6980234366203b1
Nikolay Amiantov <ab@fmap.me>
2017-08-04 19:28:58 +0300

    Fix protocol

--- solution/src/DNIWE/Punt/Interface/Online.hs
+++ solution/src/DNIWE/Punt/Interface/Online.hs
@@ -1,6 +1,9 @@
 module DNIWE.Punt.Interface.Online where
 
+import Data.Monoid
+import Data.Attoparsec.ByteString.Char8 as P
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Aeson as JSON
 import qualified Data.Aeson.Parser as JSONP
@@ -11,4 +14,8 @@ import Data.Conduit.Attoparsec
 
 runJSONClient :: ByteString -> Int -> Conduit JSON.Value IO JSON.Value -> IO ()
 runJSONClient host port conduit = runTCPClient (clientSettings port host) $ \appData -> do
-  appSource appData $$ conduitParser JSONP.json =$= CL.map snd =$= conduit =$= CL.map (BL.toStrict . JSON.encode) =$= appSink appData
+  appSource appData $$ conduitParser (P.decimal >> P.char ':' >> JSONP.json) =$= CL.map snd =$= conduit =$= CL.map encodeValue =$= appSink appData
+
+encodeValue :: JSON.Value -> ByteString
+encodeValue val = B.pack (show $ B.length encoded) <> ":" <> encoded
+  where encoded = BL.toStrict $ JSON.encode val
