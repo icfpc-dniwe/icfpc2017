f7b5ed4752a3ba51f9b2a482b3ff8fe22d211c9b
vuvko <notvuvko@gmail.com>
2017-08-07 13:06:48 +0300

    Fixed training

--- src/neural_estimator/main.py
+++ src/neural_estimator/main.py
@@ -52,7 +52,7 @@ def main(t):
 
 def prep():
     def net_player(name, length, history_coeff=0.5, net=None, a=None, u=None, save_iter=50):
-        if net is None or a is None or u is None:
+        if net is None:
             net, a, u = GetProbFunctions(10)
         return NetworkPlayer(net, a, u, length, history_coeff, save_iter=save_iter, experiment_name='test_middle' + name)
     # game = GenerateGame(2, 2, 'small')
@@ -61,38 +61,38 @@ def prep():
     # players = [net_player('_small0', 1, 0, net, a, u, 500), RandomPlayer()]
     # winners = TrainPlayers(game, players, 10000, 0.0, echo_idx=1000)
     # print('_small0:', Counter(winners))
-    net2, a2, u2 = GetProbFunctions(10)
+    net2, a2, u2 = GetProbFunctions(10, learning_rate=1e-5)
     # LoadNet(net2['desc'], 'weights/test_middle_small0_4999.npz')
     # players = [net_player('_small1', 2, 0.5, net, a, u, 500),
     #            net_player('05', 1, 0, net2, a2, None, save_iter=10000)]
     # winners = TrainPlayers(game, players, 10000, 0.00, echo_idx=1000)
     # print('_small1:', Counter(winners))
-    game = GenerateGame(2, 4, 'newman')
-    net, a, u = GetProbFunctions(10, learning_rate=1e-4)
-    LoadNet(net['desc'], 'weights/test_middle_newman0_309.npz')
-    LoadNet(net2['desc'], 'weights/test_middle_newman0_209.npz')
-    players = [net_player('_newman00', 10, 0.8, net, a, u, save_iter=10),
-               net_player('_newman1', 4, 0.9, net2, a2, None, save_iter=100000)]
-    winners = TrainPlayers(game, players, 100, 0.0, echo_idx=10)
-    print('_newman0:', Counter(winners))
+    # game = GenerateGame(2, 4, 'newman')
+    # net, a, u = GetProbFunctions(10, learning_rate=1e-4)
+    # LoadNet(net['desc'], 'weights/test_middle_newman0_309.npz')
+    # LoadNet(net2['desc'], 'weights/test_middle_newman0_209.npz')
+    # players = [net_player('_newman00', 10, 0.8, net, a, u, save_iter=10),
+    #            net_player('_newman1', 4, 0.9, net2, a2, None, save_iter=100000)]
+    # winners = TrainPlayers(game, players, 100, 0.0, echo_idx=10)
+    # print('_newman0:', Counter(winners))
+    # net, a, u = GetProbFunctions(10, learning_rate=1e-5)
+    # LoadNet(net['desc'], 'weights/test_middle_newman00_99.npz')
+    # LoadNet(net2['desc'], 'weights/test_middle_newman00_99.npz')
+    # players = [net_player('_newman1', 10, 0.8, net, a, u, save_iter=10),
+    #            net_player('_newman2', 4, 0.9, net2, a2, None, save_iter=100000)]
+    # winners = TrainPlayers(game, players, 1000, 0.0, echo_idx=10)
+    # print('_newman1:', Counter(winners))
     net, a, u = GetProbFunctions(10, learning_rate=1e-5)
-    LoadNet(net['desc'], 'weights/test_middle_newman00_99.npz')
-    LoadNet(net2['desc'], 'weights/test_middle_newman00_99.npz')
-    players = [net_player('_newman1', 10, 0.8, net, a, u, save_iter=10),
-               net_player('_newman2', 4, 0.9, net2, a2, None, save_iter=100000)]
-    winners = TrainPlayers(game, players, 1000, 0.0, echo_idx=10)
-    print('_newman1:', Counter(winners))
-    net, a, u = GetProbFunctions(10, learning_rate=1e-4)
+    net3, a3, u3 = GetProbFunctions(10, learning_rate=1e-5)
     LoadNet(net['desc'], 'weights/test_middle_newman1_999.npz')
     LoadNet(net2['desc'], 'weights/test_middle_newman1_999.npz')
-    net3, a3, u3 = GetProbFunctions(10)
-    LoadNet(net3['desc'], 'weights/test_middle_newman0_999.npz')
+    LoadNet(net3['desc'], 'weights/test_middle_newman1_999.npz')
     game = GenerateGame(4, 5, 'newman')
-    players = [net_player('_newman00', 10, 0.8, net, a, u, save_iter=5),
-               net_player('_newman11', 4, 0.9, net2, a2, u2, save_iter=5),
-               net_player('_newman22', 16, 0.8, net3, a3, u3, save_iter=5),
+    players = [net_player('_newman000', 10, 0.8, net, a, u, save_iter=5),
+               net_player('_newman111', 4, 0.9, net2, a2, u2, save_iter=5),
+               net_player('_newman222', 16, 0.8, net3, a3, u3, save_iter=5),
                RandomPlayer()]
-    winners = TrainPlayers(game, players, 200, 0.0, echo_idx=10)
+    winners = TrainPlayers(game, players, 200, 0.0, echo_idx=5)
     print('_newman2:', Counter(winners))
 
 
