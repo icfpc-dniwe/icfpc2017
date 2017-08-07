cfaf7bfe8f6c2cd5493862316192654ef7a01fce
alvearian <am-vi@yandex.ru>
2017-08-07 05:57:14 +0300

    INTEROPERABILITY

--- solution/default.nix
+++ solution/default.nix
@@ -1,8 +1,9 @@
-{ mkDerivation, aeson, attoparsec, base, base64-bytestring, binary
-, bytestring, cereal, conduit, conduit-extra, containers
-, data-default, data-default-class, deepseq, directory, fgl
-, filepath, hashable, hspec, MonadRandom, QuickCheck, stdenv, text
-, transformers, unordered-containers, vector
+{ mkDerivation, aeson, aeson-casing, attoparsec, base
+, base64-bytestring, binary, bytestring, cereal, conduit
+, conduit-extra, containers, data-default, data-default-class
+, deepseq, directory, fgl, filepath, hashable, hspec, MonadRandom
+, QuickCheck, stdenv, text, transformers, unordered-containers
+, vector
 }:
 mkDerivation {
   pname = "solution";
@@ -16,8 +17,9 @@ mkDerivation {
     hashable MonadRandom text unordered-containers
   ];
   executableHaskellDepends = [
-    aeson base bytestring cereal conduit containers data-default
-    data-default-class fgl transformers vector
+    aeson aeson-casing base bytestring cereal conduit containers
+    data-default data-default-class fgl transformers
+    unordered-containers vector
   ];
   testHaskellDepends = [
     aeson base bytestring containers directory filepath hspec
